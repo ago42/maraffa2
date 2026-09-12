91f00013225338e13f8978a0c9b1d3eb3d884627
ago42 <aagostini043@gmail.com>
2026-09-12 19:00:12 +0200

    Refactor PartitaBriscola class and add scoring logic

--- game_logic.py
+++ game_logic.py
@@ -1,126 +1,244 @@
 import random
 
+
+def ordi(l):
+  l.sort()
+  diz = {}
+  for x in l:
+    s = x.split(" ")
+    if s[1] == "Asso":
+      diz.update({x: 8})
+    elif s[1] == "2":
+      diz.update({x: 9})
+    elif s[1] == "3":
+      diz.update({x: 10})
+    elif s[1] == "4":
+      diz.update({x: 1})
+    elif s[1] == "5":
+      diz.update({x: 2})
+    elif s[1] == "6":
+      diz.update({x: 3})
+    elif s[1] == "7":
+      diz.update({x: 4})
+    elif s[1] == "Fante":
+      diz.update({x: 5})
+    elif s[1] == "Cavallo":
+      diz.update({x: 6})
+    elif s[1] == "Re":
+      diz.update({x: 7})
+  return diz
+
+
+def conta(l):
+  somma = 0
+  for x in l:
+    if x == "4" or x == "5" or x == "6" or x == "7":
+      somma = somma
+    elif x == "Asso":
+      somma = somma + 3
+    else:
+      somma = somma + 1
+  return somma / 3
+
+
 class PartitaBriscola:
-    def __init__(self):
-        self.giocatori_connessi = 0
-        self.reset_partita()
 
-    def reset_partita(self):
-        # Valori delle carte per il calcolo dei punti
-        self.valori_punti = {
-            'Asso': 11, '3': 10, 'Re': 4, 'Cavallo': 3, 'Fante': 2,
-            '7': 0, '6': 0, '5': 0, '4': 0, '2': 0
-        }
-        
-        # Gerarchia di presa della singola carta
-        self.gerarchia = {
-            'Asso': 10, '3': 9, 'Re': 8, 'Cavallo': 7, 'Fante': 6,
-            '7': 5, '6': 4, '5': 3, '4': 2, '2': 1
-        }
+  def __init__(self):
+    self.giocatori_connessi = 0
+    self.turni = 0
+    self.sommatot1 = 0
+    self.sommatot2 = 0
+    self.reset_partita()
 
-        # Creazione del mazzo da 40 carte
-        semi = ['Denari', 'Coppe', 'Spade', 'Bastoni']
-        valori = list(self.valori_punti.keys())
-        self.mazzo = [f"{seme} {valore}" for seme in semi for valore in valori]
-        random.shuffle(self.mazzo)
+  def reset_partita(self):
+    self.mano_g1 = []
+    self.mano_g2 = []
+    self.punti1 = []
+    self.punti2 = []
+    self.q = 0
 
-        # Distribuzione iniziale (3 carte a testa)
-        self.mano_g1 = [self.mazzo.pop() for _ in range(3)]
-        self.mano_g2 = [self.mazzo.pop() for _ in range(3)]
+    self.carte = [
+        "Denari Asso",
+        "Denari 2",
+        "Denari 3",
+        "Denari 4",
+        "Denari 5",
+        "Denari 6",
+        "Denari 7",
+        "Denari Fante",
+        "Denari Cavallo",
+        "Denari Re",
+        "Coppe Asso",
+        "Coppe 2",
+        "Coppe 3",
+        "Coppe 4",
+        "Coppe 5",
+        "Coppe 6",
+        "Coppe 7",
+        "Coppe Fante",
+        "Coppe Cavallo",
+        "Coppe Re",
+        "Spade Asso",
+        "Spade 2",
+        "Spade 3",
+        "Spade 4",
+        "Spade 5",
+        "Spade 6",
+        "Spade 7",
+        "Spade Fante",
+        "Spade Cavallo",
+        "Spade Re",
+        "Bastoni Asso",
+        "Bastoni 2",
+        "Bastoni 3",
+        "Bastoni 4",
+        "Bastoni 5",
+        "Bastoni 6",
+        "Bastoni 7",
+        "Bastoni Fante",
+        "Bastoni Cavallo",
+        "Bastoni Re",
+    ]
 
-        self.briscola = None
-        self.prio = 1  # 1: Turno di G1, 2: Turno di G2
-        self.punti_g1 = 0
-        self.punti_g2 = 0
-        
-        # Carte attualmente sul tavolo nel turno corrente
-        self.carta1_in_tavola = None
-        self.carta2_in_tavola = None
-        self.fase = "IN_CORSO"
+    # Distribuzione iniziale (10 carte a testa come nel tuo codice)
+    for a in range(0, 10):
+      x = random.randint(0, 39 - self.q)
+      self.mano_g1.append(self.carte[x])
+      self.carte.pop(x)
+      self.q += 1
 
-    def imposta_briscola(self, seme):
-        """Imposta il seme di briscola scelto dal Giocatore 1"""
-        self.briscola = seme
+    for a in range(0, 10):
+      x = random.randint(0, 39 - self.q)
+      self.mano_g2.append(self.carte[x])
+      self.carte.pop(x)
+      self.q += 1
 
-    def gioca_carta_singola(self, num_giocatore, carta):
-        """Gestisce la giocata di una carta da uno dei due schermi"""
-        if self.fase == "FINITA":
-            return
+    self.ordg1 = ordi(self.mano_g1)
+    self.ordg2 = ordi(self.mano_g2)
 
-        # Il Giocatore 1 gioca la sua carta
-        if num_giocatore == 1 and not self.carta1_in_tavola:
-            if carta in self.mano_g1:
-                self.mano_g1.remove(carta)
-                self.carta1_in_tavola = carta
+    self.briscole = None
+    self.prio = 1
+    self.carta1_in_tavola = None
+    self.carta2_in_tavola = None
+    self.fase = "IN_CORSO"
 
-        # Il Giocatore 2 gioca la sua carta
-        elif num_giocatore == 2 and not self.carta2_in_tavola:
-            if carta in self.mano_g2:
-                self.mano_g2.remove(carta)
-                self.carta2_in_tavola = carta
+  def imposta_briscola(self, seme):
+    self.briscole = seme
 
-        # Se entrambi i giocatori hanno giocato la carta, risolvi la mano
-        if self.carta1_in_tavola and self.carta2_in_tavola:
-            self.risolvi_turno()
+  def gioca_carta_singola(self, num_giocatore, carta):
+    if self.fase == "FINITA":
+      return
 
-    def risolvi_turno(self):
-        """Confronta le carte in tavola, assegna i punti e distribuisce nuove carte"""
-        seme1, val1 = self.carta1_in_tavola.split(' ')
-        seme2, val2 = self.carta2_in_tavola.split(' ')
+    # Registra la giocata dal rispettivo schermo
+    if num_giocatore == 1 and not self.carta1_in_tavola:
+      if carta in self.mano_g1:
+        self.carta1_in_tavola = carta
+    elif num_giocatore == 2 and not self.carta2_in_tavola:
+      if carta in self.mano_g2:
+        self.carta2_in_tavola = carta
 
-        punti_mano = self.valori_punti[val1] + self.valori_punti[val2]
-        vincitore_mano = 1
+    # Quando entrambi hanno tirato, risolve la mano con la tua logica esatta
+    if self.carta1_in_tavola and self.carta2_in_tavola:
+      self.risolvi_turno()
 
-        # Logica per determinare chi vince la mano
-        if seme1 == seme2:
-            if self.gerarchia[val2] > self.gerarchia[val1]:
-                vincitore_mano = 2
-        elif seme2 == self.briscola:
-            vincitore_mano = 2
+  def risolvi_turno(self):
+    carta1 = self.carta1_in_tavola
+    carta2 = self.carta2_in_tavola
 
-        # Se il primo a giocare nel turno era G2, invertiamo la logica di partenza
-        if self.prio == 2:
-            vincitore_mano = 2 if vincitore_mano == 1 else 1
+    self.mano_g1.remove(carta1)
+    self.mano_g2.remove(carta2)
+    if carta1 in self.ordg1:
+      del self.ordg1[carta1]
+    if carta2 in self.ordg2:
+      del self.ordg2[carta2]
 
-        # Assegnazione punti e aggiornamento priorità di gioco
-        if vincitore_mano == 1:
-            self.punti_g1 += punti_mano
-            self.prio = 1
-        else:
-            self.punti_g2 += punti_mano
-            self.prio = 2
+    carta1s = carta1.split(" ")
+    carta2s = carta2.split(" ")
 
-        # Resetta il tavolo
-        self.carta1_in_tavola = None
-        self.carta2_in_tavola = None
+    if self.prio == 1:
+      if carta1s[0] == carta2s[0]:
+        if self.ordg1[carta1] > self.ordg2[carta2]:
+          self.punti1.append(carta1s[1])
+          self.punti1.append(carta2s[1])
+          self.prio = 1
+        elif self.ordg1[carta1] < self.ordg2[carta2]:
+          self.punti2.append(carta1s[1])
+          self.punti2.append(carta2s[1])
+          self.prio = 2
+      elif carta2s[0] == self.briscole:
+        self.punti2.append(carta1s[1])
+        self.punti2.append(carta2s[1])
+        self.prio = 2
+      else:
+        self.punti1.append(carta1s[1])
+        self.punti1.append(carta2s[1])
+        self.prio = 1
+    else:
+      if carta2s[0] == carta1s[0]:
+        if self.ordg2[carta2] > self.ordg1[carta1]:
+          self.punti2.append(carta2s[1])
+          self.punti2.append(carta1s[1])
+          self.prio = 2
+        elif self.ordg2[carta2] < self.ordg1[carta1]:
+          self.punti1.append(carta1s[1])
+          self.punti1.append(carta2s[1])
+          self.prio = 1
+      elif carta1s[0] == self.briscole:
+        self.punti1.append(carta1s[1])
+        self.punti1.append(carta2s[1])
+        self.prio = 1
+      else:
+        self.punti2.append(carta1s[1])
+        self.punti2.append(carta2s[1])
+        self.prio = 2
 
-        # Pesca di nuove carte dal mazzo (se ce ne sono)
-        if len(self.mazzo) >= 2:
-            if self.prio == 1:
-                self.mano_g1.append(self.mazzo.pop())
-                self.mano_g2.append(self.mazzo.pop())
-            else:
-                self.mano_g2.append(self.mazzo.pop())
-                self.mano_g1.append(self.mazzo.pop())
+    # Pesca nuova carta se ce ne sono ancora
+    if len(self.carte) >= 2:
+      x = random.randint(0, 39 - self.q)
+      self.mano_g1.append(self.carte[x])
+      self.q += 1
+      self.carte.pop(x)
 
-        # Controlla la fine della partita
-        if not self.mano_g1 and not self.mano_g2:
-            self.fase = "FINITA"
+      x = random.randint(0, 39 - self.q)
+      self.mano_g2.append(self.carte[x])
+      self.q += 1
+      self.carte.pop(x)
 
-    def to_dict(self):
-        """Converte lo stato del gioco in dizionario per inviarlo al browser via JSON"""
-        return {
-            "mano_g1": self.mano_g1,
-            "mano_g2": self.mano_g2,
-            "carta1_in_tavola": self.carta1_in_tavola,
-            "carta2_in_tavola": self.carta2_in_tavola,
-            "briscole": self.briscola,
-            "prio": self.prio,
-            "carte_rimaste_mazzo": len(self.mazzo),
-            "punti_g1": self.punti_g1,
-            "punti_g2": self.punti_g2,
-            "fase": self.fase
-        } 
+      self.ordg1 = ordi(self.mano_g1)
+      self.ordg2 = ordi(self.mano_g2)
+
+    self.carta1_in_tavola = None
+    self.carta2_in_tavola = None
+
+    # Fine mano: calcola i punti esattamente con la tua funzione conta()
+    if not self.mano_g1 and not self.mano_g2:
+      if self.prio == 1:
+        somma1 = conta(self.punti1) + 1
+        somma2 = conta(self.punti2)
+      else:
+        somma2 = conta(self.punti2) + 1
+        somma1 = conta(self.punti1)
+
+      self.sommatot1 += somma1
+      self.sommatot2 += somma2
+      self.turni += 1
+      self.fase = "FINITA"
+
+  def to_dict(self):
+    return {
+        "mano_g1": self.mano_g1,
+        "mano_g2": self.mano_g2,
+        "carta1_in_tavola": self.carta1_in_tavola,
+        "carta2_in_tavola": self.carta2_in_tavola,
+        "briscole": self.briscole,
+        "prio": self.prio,
+        "carte_rimaste_mazzo": len(self.carte),
+        "punti_g1": conta(self.punti1),
+        "punti_g2": conta(self.punti2),
+        "sommatot1": self.sommatot1,
+        "sommatot2": self.sommatot2,
+        "fase": self.fase,
+    }
                     
                  
             
